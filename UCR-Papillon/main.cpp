891eb8c830290cd37fc031b4ece0bc707972cf6d
William Thurston <william@foozlegame.com>
2012-01-19 19:46:05 -0800

    Pushing a file...

---
+++ UCR-Papillon/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UCR-Papillon
+//
+//  Created by William Thurston on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    cout << "Some stuffs;
+    return 0;
+}
